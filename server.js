56c313de2e749e3b761e8a81e07b14eae5a9a963
siddharthsachdeva2835 <siddharthsachdeva2835@gmail.com>
2019-04-09 12:58:20 +0530

    completed

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const mysql =  require('mysql')
 const app = express()
 const port =   process.env.PORT || 3000
 
@@ -7,11 +8,36 @@ const port =   process.env.PORT || 3000
 // client will call localhost:3000/css/style.css to get a static file
 app.use(express.static('public'))
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded())
+
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jade')
+
+const connection = mysql.createConnection({
+    host     : process.env.RDS_HOSTNAME,
+    user     : process.env.RDS_USERNAME,
+    password : process.env.RDS_PASSWORD,
+    port     : process.env.RDS_PORT
+})
+
+connection.connect(function(err) {
+    if (err) {
+      console.error('Database connection failed: ' + err.stack);
+      return;
+    }
+  
+    console.log('Connected to database.');
+});
+  
+
 
 app.get('/', (req,res) => {
-    res.sendFile('views/index.html', {root: __dirname})
+    connection.query('SELECT * FROM user.login', function (error, results, fields) {
+        if (error) throw error
+        
+        res.render('index', { name: results[0].name})
+    });
 })
 
 app.listen(port, () => {
